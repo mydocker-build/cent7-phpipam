c0f26fdbef70cf9f83719181765fb043a566351d
root <root@pslab-lxc01.prosethsolutions.lab>
2018-09-10 16:15:37 +0700

    Update phpIPAM configuration

--- conf.d/run-httpd.sh
+++ conf.d/run-httpd.sh
@@ -14,17 +14,6 @@ else
     chown -R apache:apache $DIR
 fi
 
-# Check configuration
-chk_conf=`grep localhost $DIR/config.php|awk '{print $3}'`
-if [ "$chk_conf" == "'localhost';" ]; then
-        echo "Configuration has been changed...!"
-        cd $DIR
-        sed -i "s/'localhost'/'$DB_PHPIPAM_HOST'/" config.php
-        sed -i "7s/'phpipam'/'$DB_PHPIPAM_USER'/" config.php
-        sed -i "s/'phpipamadmin'/'$DB_PHPIPAM_PASSWORD'/" config.php
-        sed -i "9s/'phpipam'/'$DB_PHPIPAM_NAME'/" config.php
-fi
-
 # Make sure we're not confused by old, incompletely-shutdown httpd
 # context after restarting the container.  httpd won't start correctly
 # if it thinks it is already running.
